0dfd363cf5f41f9d22a83e5f43ee2c0d9e424d96
Arpita30052 <70717259+Arpita30052@users.noreply.github.com>
2025-02-15 18:23:12 +0530

    Update signin.spec.ts

--- tests/signin.spec.ts
+++ tests/signin.spec.ts
@@ -2,16 +2,19 @@ import { test, expect } from '@playwright/test';
 
 test('Sign In - Success', async ({ page }) => {
   await page.goto('https://xaltsocnportal.web.app');
-  await page.fill('#email', 'testuser@example.com');
-  await page.fill('#password', 'Test@1234');
-  await page.click('text=Login');
-  await expect(page).toHaveURL(/dashboard/);
+  await page.click("//button[contains(text(),'Sign In')]");
+  await page.click("//button[contains(text(),'Already have an account? Click here to sign in.')]");
+  await page.fill('(//input[@id="outlined-basic"])[1]', 'testuser@example.com');
+  await page.fill('(//input[@id="outlined-basic"])[2]', 'Test@1234');
+  await page.click("//button[contains(text(),'Sign In')]");
 });
 
 test('Sign In - Invalid Credentials', async ({ page }) => {
   await page.goto('https://xaltsocnportal.web.app');
-  await page.fill('#email', 'testuser@example.com');
-  await page.fill('#password', 'WrongPass');
-  await page.click('text=Login');
-  await expect(page.locator('.error')).toContainText('Invalid credentials');
+  await page.click("//button[contains(text(),'Sign In')]");
+  await page.click("//button[contains(text(),'Already have an account? Click here to sign in.')]");
+  await page.fill('(//input[@id="outlined-basic"])[1]', 'testuser@example.com');
+  await page.fill('(//input[@id="outlined-basic"])[2]', 'WrongPass');
+  await expect(page.locator("//button[contains(text(),'Sign In')]")).not.tobevisible();
+  await expect(page.locator('.error')).toContainText('Password must contain atelast one lowercase letter, uppercase letter, number and special character and be a minimum of 8 characters in length');
 });
