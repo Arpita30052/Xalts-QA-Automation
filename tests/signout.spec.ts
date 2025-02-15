91b824530352e36c7aa19e868edacc4c26257070
Arpita30052 <70717259+Arpita30052@users.noreply.github.com>
2025-02-15 17:42:55 +0530

    Update signout.spec.ts

--- tests/signout.spec.ts
+++ tests/signout.spec.ts
@@ -1,1 +1,10 @@
+import { test, expect } from '@playwright/test';
 
+test('Sign Out', async ({ page }) => {
+  await page.goto('https://xaltsocnportal.web.app');
+  await page.fill('#email', 'testuser@example.com');
+  await page.fill('#password', 'Test@1234');
+  await page.click('text=Login');
+  await page.click('text=Sign Out');
+  await expect(page).toHaveURL(/login/);
+});
