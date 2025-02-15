c1742b49ef5d1285a9997f796ffdb52e711c77ac
Arpita30052 <70717259+Arpita30052@users.noreply.github.com>
2025-02-15 18:15:06 +0530

    Update signup.spec.ts

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -2,18 +2,19 @@ import { test, expect } from '@playwright/test';
 
 test('Sign Up - Success', async ({ page }) => {
   await page.goto('https://xaltsocnportal.web.app');
-  await page.click('text=Sign Up');
-  await page.fill('#email', 'testuser@example.com');
-  await page.fill('#password', 'Test@1234');
-  await page.click('text=Submit');
+  await page.click("//button[contains(text(),'Sign In')]");
+  await page.fill('(//input[@id="outlined-basic"])[1]', 'testuser@example.com');
+  await page.fill('(//input[@id="outlined-basic"])[2]', 'Test@1234');
+  await page.fill('(//input[@id="outlined-basic"])[3]', 'Test@1234');
+  await page.click("//button[contains(text(),'Sign Up')]");
   await expect(page).toHaveURL(/dashboard/);
 });
 
 test('Sign Up - Invalid Email', async ({ page }) => {
   await page.goto('https://xaltsocnportal.web.app');
-  await page.click('text=Sign Up');
-  await page.fill('#email', 'invalid-email');
-  await page.fill('#password', 'Test@1234');
-  await page.click('text=Submit');
-  await expect(page.locator('.error')).toContainText('Invalid email');
+  await page.click("//button[contains(text(),'Sign In')]");
+  await page.fill('(//input[@id="outlined-basic"])[1]', 'invalidemail@com');
+  await page.fill((//input[@id="outlined-basic"])[2]', 'Test@1234');
+  await page.fill('(//input[@id="outlined-basic"])[3]', 'Test@1234');
+  await expect(page.locator("//button[contains(text(),'Sign Up')]")).not.tobevisible();
 });
