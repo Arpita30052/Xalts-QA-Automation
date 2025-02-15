31ab8f80d89d76c245e363dea58454b56f65ba52
Arpita30052 <70717259+Arpita30052@users.noreply.github.com>
2025-02-15 18:27:09 +0530

    Update signout.spec.ts

--- tests/signout.spec.ts
+++ tests/signout.spec.ts
@@ -2,9 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('Sign Out', async ({ page }) => {
   await page.goto('https://xaltsocnportal.web.app');
-  await page.fill('#email', 'testuser@example.com');
-  await page.fill('#password', 'Test@1234');
-  await page.click('text=Login');
-  await page.click('text=Sign Out');
-  await expect(page).toHaveURL(/login/);
+  await page.click("//button[contains(text(),'Sign In')]");
+  await page.click("//button[contains(text(),'Already have an account? Click here to sign in.')]");
+  await page.fill('(//input[@id="outlined-basic"])[1]', 'testuser@example.com');
+  await page.fill('(//input[@id="outlined-basic"])[2]', 'Test@1234');
+  await page.click("//button[contains(text(),'Sign In')]");
+  await expect(page.locator("//button[contains(text(),'Sign Out')]")).tobevisible();
+  await page.click("//button[contains(text(),'Sign Out')]");
+  await expect(page.locator("//button[contains(text(),'Sign In')]")).tobevisible();
 });
